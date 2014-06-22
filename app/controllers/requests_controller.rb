ac9a189b4a3fcf9f393786f6546376504e75c8ca
Vladimir <markovnin@gmail.com>
2014-06-22 11:14:03 +0400

    Запрос получения списка справочников

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -3,4 +3,28 @@ class RequestsController < ApplicationController
     @request = Request.new
     @queries = Query.order(:name)
   end
+  
+  def create
+    case Rails.env
+      when 'development' then url = 'priem.edu.ru:8000'
+      when 'production' then url = '127.0.0.1:8080'
+    end
+    method = '/' + Query.find(params[:request][:query_id]).name
+    request = Request.data(method)
+    uri = URI.parse('http://' + url + '/import/importservice.svc')
+    http = Net::HTTP.new(uri.host, uri.port)
+    headers = {'Content-Type' => 'text/xml'}
+    response = http.post(uri.path + method, request, headers)
+    @request = Request.new(query_id: params[:request][:query_id], input: request, output: response.body)
+
+    respond_to do |format|
+      if @request.save
+        format.html { redirect_to @request, notice: 'Request was successfully created.' }
+        format.json { render json: @request, status: :created, location: @request }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @request.errors, status: :unprocessable_entity }
+      end
+    end
+  end
 end
--- app/models/request.rb
+++ app/models/request.rb
@@ -1,3 +1,23 @@
 class Request < ActiveRecord::Base
+  require 'builder'
   belongs_to :query
+  
+  def self.data(method)
+    case method
+    when '/dictionary'
+      data = ::Builder::XmlMarkup.new(indent: 2)
+      data.Root do |root|
+        auth_data(root)
+      end
+    end
+  end
+  
+  def self.auth_data(root)
+    auth_data = ::Builder::XmlMarkup.new(indent: 2)
+    a = AuthData.first
+    root.AuthData do |ad|
+      ad.Login a.login
+      ad.Pass a.pass
+    end
+  end
 end
