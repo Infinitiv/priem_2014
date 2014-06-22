170dcd9614c08f29ceae75360ff4ec8526ce2fa2
Vladimir <markovnin@gmail.com>
2014-06-22 11:36:46 +0400

    Отображение отдельного запроса

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,4 +1,8 @@
 class RequestsController < ApplicationController
+  before_action :set_request, only: [:show]
+  def show
+    
+  end
   def new
     @request = Request.new
     @queries = Query.order(:name)
@@ -20,11 +24,15 @@ class RequestsController < ApplicationController
     respond_to do |format|
       if @request.save
         format.html { redirect_to @request, notice: 'Request was successfully created.' }
-        format.json { render json: @request, status: :created, location: @request }
       else
         format.html { render action: "new" }
-        format.json { render json: @request.errors, status: :unprocessable_entity }
       end
     end
+  end
+  
+  private
+  
+  def set_request
+    @request = Request.find params[:id]
   end
 end
