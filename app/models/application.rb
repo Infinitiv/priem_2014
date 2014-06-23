4fb73b1b20b161527c337393c15884f7eb6dd803
Vladimir <markovnin@gmail.com>
2014-06-23 22:41:12 +0400

    Заготовка модели для заявлений

---
+++ app/models/application.rb
@@ -0,0 +1,29 @@
+class Application < ActiveRecord::Base
+  
+  belongs_to :campaign
+
+  def self.import(file)
+    spreadsheet = open_spreadsheet(file)
+    header = spreadsheet.row(1)
+    (2..spreadsheet.last_row).each do |i|
+      row = Hash[[header, spreadsheet.row(i)].transpose]
+      application = find_by_application_number(row["application_number"]) || new
+      application.attributes = row.to_hash.slice(*accessible_attributes)
+      application.save!
+    end
+  end
+
+  def self.open_spreadsheet(file)
+    case File.extname(file.original_filename)
+    when ".ods" then Roo::Openoffice.new(file.path, nil, :ignore)
+    when ".csv" then Roo::Csv.new(file.path, nil, :ignore)
+    when ".xls" then Roo::Excel.new(file.path, nil, :ignore)
+    when ".xlsx" then Roo::Excelx.new(file.path, nil, :ignore)
+    else raise "Unknown file type: #{file.original_filename}"
+    end
+  end
+  
+  def fio
+    [entrant_last_name, entrant_first_name, entrant_middle_name].compact.join(' ')
+  end
+end
