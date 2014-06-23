9fb7c94238c8ff35db6f9fe8e3f56edf4c690a5f
Vladimir <markovnin@gmail.com>
2014-06-23 22:36:36 +0400

    Маршруты для заявлений

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,11 @@ Rails.application.routes.draw do
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
   resources :requests
+  resources :appications do
+    collection do
+      post 'import'
+    end
+  end
   # Example resource route with options:
   #   resources :products do
   #     member do
