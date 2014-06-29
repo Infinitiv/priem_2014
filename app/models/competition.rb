e172e6b0bc44084b7bc54f0134933bbb36897d0a
Vladimir <markovnin@gmail.com>
2014-06-29 16:19:19 +0400

    Удаление конкурсов перед импортом новых

--- app/models/competition.rb
+++ app/models/competition.rb
@@ -5,6 +5,9 @@ class Competition < ActiveRecord::Base
   def self.import_from_row(row, application)
     competition_items = CompetitionItem.all
     competitions = application.competitions
+    competitions.each do |competition|
+      competition.destroy
+    end
     competition_items.each do |competition_item|
       competition = competitions.where(id: "c_#{competition_item.id}").first || competitions.new if row["c_#{competition_item.id}"]
       competition.update_attributes(competition_item_id: competition_item.id, priority: row["c_#{competition_item.id}"]) if row["c_#{competition_item.id}"]
