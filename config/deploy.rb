f6cd5de3e27554ca73c310c9fd6ab151f30df05a
Vladimir <markovnin@gmail.com>
2014-07-06 19:55:02 +0400

    измения настроек деплоя

--- config/deploy.rb
+++ config/deploy.rb
@@ -41,8 +41,7 @@ namespace :deploy do
     on roles(:app), in: :sequence, wait: 5 do
       # Your restart mechanism here, for example:
       # execute :touch, release_path.join('tmp/restart.txt')
-      set :password, 'user'
-      execute "sudo service thin restart"
+      execute "service thin restart"
     end
   end
 
