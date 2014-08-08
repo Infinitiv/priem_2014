a0f5f69eb1dbb4c20a8ce9e50320429272adc7ac
Vladimir Markovnin <markovnin@gmail.com>
2014-08-08 14:33:51 +0400

    Формирование списков зачисленных по оригиналам документов

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -22,8 +22,8 @@ before_action :set_application, only: [:show]
   end
   
   def competition
-    @applications = Application.includes(:competitions).where(campaign_id: @default_campaign, last_deny_day: nil).where("chemistry > ? and biology > ? and russian > ?", 35, 35, 35).sort_by{|a| [a.summa, a.chemistry, a.biology, a.russian]}.reverse
-    @admission_volume = AdmissionVolume.where(campaign_id: @default_campaign)
+    @applications = Application.includes(:competitions).where(campaign_id: 2, last_deny_day: nil).where("chemistry > ? and biology > ? and russian > ?", 35, 35, 35).where.not(original_received_date: nil).sort_by{|a| [a.summa, a.chemistry, a.biology, a.russian]}.reverse
+    @admission_volume = AdmissionVolume.where(campaign_id: 2)
     @applications_hash = Application.competition(@applications, @admission_volume)
   end
   
