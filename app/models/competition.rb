e9a82d193ee07b07eaba9b6a910140dac74459bb
Vladimir <markovnin@gmail.com>
2015-07-04 10:41:17 +0400

    Target ranked

--- app/models/competition.rb
+++ app/models/competition.rb
@@ -24,6 +24,16 @@ class Competition < ActiveRecord::Base
     "(место в конкурсе - #{rank_all(campaign_id, summa, applications)}, с учетом оригиналов - #{rank_original_only(campaign_id, summa, applications)})"
   end
   
+  def rank_target
+    campaign_id = application.campaign_id
+    target_organization_id = application.target_organization_id
+    summa = application.summa + application.achiev_summa
+    applications = Application.where(campaign_id: campaign_id).joins(:institution_achievements).map(&:id)
+    marks = Mark.joins(:competitions).where(competitions: {competition_item_id: competition_item_id}, applications: {campaign_id: campaign_id, target_organization_id: target_organization_id}).where.not(applications: {original_received_date: nil}).group_by(&:application_id).map{|a, ms| applications.include?(a) ? ms.map{|m| m.value}.sum + 10 : ms.map{|m| m.value}.sum}.sort.reverse
+    marks_count = marks.count{|x| x == summa}
+    marks_count > 1 ? "место в конкурсе - #{marks.index(summa) + 1}-#{marks.index(summa) + 1 + marks_count}" : "место в конкурсе #{marks.index(summa) + 1}"
+  end
+  
   def rank_all(campaign_id, summa, applications)
     marks = Mark.joins(:competitions).where(competitions: {competition_item_id: competition_item_id}, applications: {campaign_id: campaign_id}).group_by(&:application_id).map{|a, ms| applications.include?(a) ? ms.map{|m| m.value}.sum + 10 : ms.map{|m| m.value}.sum}.sort.reverse
     marks_count = marks.count{|x| x == summa}
