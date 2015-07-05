ef0858ee6c2b9d6c3a40b89379dd5a105337507a
Vladimir <markovnin@gmail.com>
2015-07-05 23:06:23 +0400

    Task to generate list of applications

--- app/models/competition_item.rb
+++ app/models/competition_item.rb
@@ -1,4 +1,7 @@
 class CompetitionItem < ActiveRecord::Base
   belongs_to :competitive_group_item
   has_one :competitive_group, through: :competitive_group_item
+  has_one :campaign, through: :competitive_group
+  has_many :competitions
+  has_many :applications, through: :competitions
 end
